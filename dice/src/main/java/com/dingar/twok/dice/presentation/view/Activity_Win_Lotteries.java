2a006ad0a3710fa48279d30c11f44de8bd391126
dagger11 <pyaehho1259@gmail.com>
2022-04-30 21:06:29 +0630

    Initial Commit

--- dice/src/main/java/com/dingar/twok/dice/presentation/view/Activity_Win_Lotteries.java
+++ dice/src/main/java/com/dingar/twok/dice/presentation/view/Activity_Win_Lotteries.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dingar.twok.dice.R;
+import com.dingar.twok.dice.presentation.contract.WinLotteryContract;
 
-public class Activity_Win_Lotteries extends AppCompatActivity {
+public class Activity_Win_Lotteries extends AppCompatActivity implements WinLotteryContract.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_lotteries);
     }
+
+
+
 }
