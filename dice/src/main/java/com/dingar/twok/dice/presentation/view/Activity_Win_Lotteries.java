61c444304d8ea90567b24ebf7ba2f3b5603d3b6f
dagger11 <pyaehho1259@gmail.com>
2022-05-01 15:05:18 +0630

    Initial Commit

--- dice/src/main/java/com/dingar/twok/dice/presentation/view/Activity_Win_Lotteries.java
+++ dice/src/main/java/com/dingar/twok/dice/presentation/view/Activity_Win_Lotteries.java
@@ -5,16 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dingar.twok.dice.R;
+import com.dingar.twok.dice.data.model.WinLotteryModel;
+import com.dingar.twok.dice.di.component.BetSlipComponentProvider;
+import com.dingar.twok.dice.di.component.WinLotteryComponent;
 import com.dingar.twok.dice.presentation.contract.WinLotteryContract;
 
+import javax.inject.Inject;
+
 public class Activity_Win_Lotteries extends AppCompatActivity implements WinLotteryContract.View {
+
+    @Inject
+    public WinLotteryContract.Presenter presenter;
+
+    WinLotteryComponent component;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_lotteries);
+
+        component = ((BetSlipComponentProvider)getApplicationContext()).provideWinLotteryComponent();
+        component.inject(this);
+
+        presenter.loadLuckyHistory();
+
     }
 
 
+    @Override
+    public void onLuckyHistoryLoaded(WinLotteryModel model) {
 
+    }
 }
